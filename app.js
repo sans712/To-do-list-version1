eeec939ec01e0b8bea4174a06b718ba00b2f3483
sanskriti <54080445+sans712@users.noreply.github.com>
2020-09-20 17:44:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,24 +45,4 @@ app.listen(3000, function () {
   console.log("server is up and running!");
 });
 
-// if(today.getDay()===6 ){
-//     day="saturday";
-// }
-// else if(today.getDay()==0){
-//     day="sunday";
-// }
-// else if(today.getDay()==1){
-//     day="monday";
-// }
-// else if(today.getDay()==2){
-//     day="tuesday";
-// }
-// else if(today.getDay()==3){
-//     day="wednesday";
-// }
-// else if(today.getDay()==4){
-//     day="thursday";
-// }
-// else if(today.getDay()==5){
-//     day="friday";
-// }
+
